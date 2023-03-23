0cbc263429d64a3a57ef238918bd604bbb9c54c4
Chira Noris Calin <norisnoris118@gmail.com>
2023-03-23 19:55:12 +0200

    #update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const theNumber = Math.floor(Math.random() * 20 + 1);
-document.querySelector(".number").textContent = theNumber;
+let theNumber = Math.floor(Math.random() * 20 + 1);
+document.querySelector(".number").textContent = "";
 let theScore = 20; //document.querySelector(".score").value;
+let highscore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   // console.log(document.querySelector(".guess").value);
@@ -34,10 +35,36 @@ document.querySelector(".check").addEventListener("click", function () {
     //When the number is correct
   } else {
     document.querySelector(".message").textContent = "Correct number!";
-    document.querySelector(".highscore").textContent = theScore;
+    if (theScore > highscore) {
+      highscore = theScore;
+
+      document.querySelector(".highscore").textContent = highscore;
+    }
     document.querySelector("body").style.backgroundColor = "#60b347";
-    document.querySelector("body").style.width = "30rem";
   }
 });
 
 console.log(theNumber);
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK 😀
+*/
+
+document.querySelector(".again").addEventListener("click", function () {
+  theScore = 20;
+  theNumber = Math.floor(Math.random() * 20 + 1);
+  document.querySelector(".message").textContent = " Start guessing...";
+  document.querySelector(".number").textContent = theNumber;
+  document.querySelector("body").style.backgroundColor = "#222";
+});
+
+//Start guessing...
